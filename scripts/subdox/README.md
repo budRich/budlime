60bd0bc4a0b110cc258bcfde36e05ebdb28df3ac
budRich <robstenklippa@gmail.com>
2018-08-09 19:40:31 +0200

    added dox list to subdox readme

--- scripts/subdox/README.md
+++ scripts/subdox/README.md
@@ -11,6 +11,14 @@ DESCRIPTION
 `subdox` uses `GTFM` to download sublime related
 documentation. See `subdox.ini` for more info.
 
+DOCUMENTATION
+-------------
+The following documentation is downloaded with `subdox`:  
+
+* SublimeLinter  
+* Sublime official  
+* Sublime unofficial  
+* Package Control  
 
 OPTIONS
 -------
--- scripts/subdox/subdox.sh
+++ scripts/subdox/subdox.sh
@@ -38,6 +38,14 @@ DESCRIPTION
 `subdox` uses `GTFM` to download sublime related
 documentation. See `subdox.ini` for more info.
 
+DOCUMENTATION
+-------------
+The following documentation is downloaded with `subdox`:  
+
+* SublimeLinter  
+* Sublime official  
+* Sublime unofficial  
+* Package Control  
 
 OPTIONS
 -------
