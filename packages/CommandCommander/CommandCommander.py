0f104c66c9e3ffa2c9c7e30010d942ee61cd79ff
budRich <robstenklippa@gmail.com>
2018-08-22 14:32:25 +0200

    added fixed command commander

--- packages/CommandCommander/CommandCommander.py
+++ packages/CommandCommander/CommandCommander.py
@@ -6,3 +6,7 @@ import os.path
 class StopInsertOnFocusLost(sublime_plugin.EventListener):
     def on_deactivated(self, view):
         view.run_command('exit_insert_mode')
+
+    def on_activated(self, view):
+        if view.settings().get('is_widget'):
+            view.run_command('enter_insert_mode')
